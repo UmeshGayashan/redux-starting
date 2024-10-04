bb48b60c446182bf6be9245d59bab483b4db62c2
Umesh Gayashan <138327662+UmeshGayashan@users.noreply.github.com>
2024-10-04 21:50:54 +0530

    create Button Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import '../App.css';
+import ButtonComponent from './ButtonComponent';
 
 function App() {
   return (
     <div className="App">
       <h1>Redux Testing</h1>
+      <ButtonComponent/>
     </div>
   );
 }
---
+++ src/components/ButtonComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ButtonComponent extends Component {
+  render() {
+    return (
+      <div>
+        <button>Click Me</button>
+      </div>
+    )
+  }
+}
