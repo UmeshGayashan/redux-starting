2472cfc67c1506e6554c4e619926ec32555ebf90
Umesh Gayashan <138327662+UmeshGayashan@users.noreply.github.com>
2024-10-04 22:47:56 +0530

    bind action with component

--- src/components/ButtonComponent.js
+++ src/components/ButtonComponent.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
+import { fetchPosts } from '../actions/postAction'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 
-export default class ButtonComponent extends Component {
+class ButtonComponent extends Component {
   render() {
     return (
       <div>
@@ -9,3 +12,9 @@ export default class ButtonComponent extends Component {
     )
   }
 }
+
+function matchDispatchToProps(dispatch){
+    return bindActionCreators({fetchPosts:fetchPosts},dispatch)
+}
+
+export default connect(null,matchDispatchToProps)(ButtonComponent)
