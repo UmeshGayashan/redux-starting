98f921f743eb09e9f77142827e26e9360544cd1f
Umesh Gayashan <138327662+UmeshGayashan@users.noreply.github.com>
2024-10-05 14:08:53 +0530

    setup newly created reducer in rootReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,11 @@
 import { combineReducers } from "redux";
 import reducer_AllPost from "./reducer_AllPost";
+import reducer_SelectedPost from "./reducer_SelectedPost";
+import { selectPost } from "../actions/postAction";
 
 const rootReducer = combineReducers({
     all_Posts:reducer_AllPost,
+    selectedPost:selectPost
 })
 
 export default rootReducer;
